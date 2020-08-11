4f415fe2d6ee2356d60034be7de31175f18d028c
Felipe Moraes <em.felipe@gmail.com>
2020-08-11 01:17:34 -0300

    feito inserção de usuario no banco

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,34 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
-routes.post('/classes', (request, response) => {
-    const data = request.body;
+routes.post('/classes', async (request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
 
-    console.log(data);
+    const insertedUsersIds =
+    await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,    
+    });
+
+    const user_id = insertedUsersIds[0];
+
+    await db('classes').insert({
+        subject,
+        cost,
+        user_id,
+    });
 
     return response.send();
 });
