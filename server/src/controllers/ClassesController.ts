6ddfd465323fc731575e94822f9d0f35d33fad33
Felipe Moraes <em.felipe@gmail.com>
2020-08-12 17:50:51 -0300

    feito query para comparação de dia da semana

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -32,7 +32,15 @@ export default class ClassesController {
 
         //consulta por matéria
         const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            })
             .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
         
         return response.json(classes);
     }    
