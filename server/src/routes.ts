766fcc0c0b0d395df71acb67a3763776f62e81db
Felipe Moraes <em.felipe@gmail.com>
2020-08-12 00:29:50 -0300

    finalizada rota para criar uma aula

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import db from './database/connection';
+import convertHourToMinutes from './utils/converHoursToMinutes';
 
 const routes = express.Router();
+
+//criação da interface do Schedule Item
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
 routes.post('/classes', async (request, response) => {
     const {
@@ -14,23 +22,57 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body;
 
-    const insertedUsersIds =
-    await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,    
-    });
+    //criação da transaction para sincronização das inserções
+    const trx = await db.transaction();
 
-    const user_id = insertedUsersIds[0];
+    try {
+        //inserção de usuários
+        const insertedUsersIds =
+        await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,    
+        });
+        
+        //armazenando id do primeiro usuario
+        const user_id = insertedUsersIds[0];
 
-    await db('classes').insert({
-        subject,
-        cost,
-        user_id,
-    });
 
-    return response.send();
+        //inserção de aulas
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+
+        //pegando id da aula
+        const class_id = insertedClassesIds[0];
+
+        //inserção de horario
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        });
+
+        await trx('class_schedule').insert(classSchedule);
+
+        await trx.commit();
+
+        return response.status(201).send();
+
+    } catch (err) {
+
+        await trx.rollback();
+        
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+    }
 });
 
 export default routes;
---
+++ server/src/utils/converHoursToMinutes.ts
@@ -0,0 +1,7 @@
+//função para conversão de horario para minutos
+export default function convertHourToMinutes(time: string) {
+    const [hour, minutes] = time.split(':').map(Number);
+    const timeInMinutes = (hour * 60) + minutes;
+    
+    return timeInMinutes;
+}
