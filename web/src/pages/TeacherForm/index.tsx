6dceb5ecbcad59fa056f4e156e1f32e9d5a1483f
Felipe Moraes <em.felipe@gmail.com>
2020-08-13 01:56:51 -0300

    estilização da página de formulário

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
@@ -16,21 +19,28 @@ function TeacherForm () {
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name"/>
-                    </div>
+                    <Input name="name" label="Nome completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
 
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type="text" id="whatsapp"/>
-                    </div>
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula" />
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br/>
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">Salvar cadastro</button>
+                </footer>
             </main>
         </div>
     )
