e661301b47130fd117a0b5965b8c4982ae9a77f4
Felipe Moraes <em.felipe@gmail.com>
2020-08-19 23:19:19 -0300

    criado estado para adição de novos horarios de aulas

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -14,10 +14,11 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
 const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
     return (
         <div className="select-block">
-            <label htmlFor={name}>{label}</label>
-            <select id={name} {...rest}>
 
-                <option value='' selected disabled hidden>Selecione uma opção</option>
+            <label htmlFor={name}>{label}</label>
+
+            <select defaultValue="" id={name} {...rest}>
+                <option value='' disabled hidden>Selecione uma opção</option>
 
                 {options.map(option => {
                     return <option key={option.value} value={option.value}> {option.label} </option>
